15b1d750cb36f7ef3f0b124a5fbbf7c201555cbe
PescadoMuerto1 <mbl0502727793@egmail.com>
2024-03-18 19:43:43 +0200

    Add crud to service

--- server.js
+++ server.js
@@ -1,4 +1,49 @@
 import express from 'express'
+import { bugService } from './services/bug.service.js'
+
 const app = express()
+
 app.get('/', (req, res) => res.send('Hello there'))
 app.listen(3030, () => console.log('Server ready at port 3030'))
+
+app.get('/api/bug', (req, res) => {
+    bugService.query()
+    .then(bugs => res.send(bugs))
+})
+
+app.get('/api/bug/save', (req, res) => {
+    const bugToSave = {
+        _id: req.query._id,
+        title: req.query.title,
+        desc: req.query.desc,
+        severity: +req.query.severity,
+        createdAt: +req.query.createdAt,
+    }
+
+    bugService.save(bugToSave)
+        .then(bug => res.send(bug))
+        .catch((err) => {
+            loggerService.error('Cannot save bug', err)
+            res.status(400).send('Cannot save bug')
+        })
+})
+
+app.get('/api/bug/:bugId', (req, res) => {
+
+    bugService.getById(req.params.bugId)
+    .then(bug => res.send(bug))
+    .catch(err => {
+        // loggerService.error(err)
+        res.status(400).send('Cannot get bug')
+    })
+})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {
+    const bugId = req.params.bugId
+    bugService.remove(bugId)
+    .then(() => res.send(bugId))
+    .catch((err) => {
+        // loggerService.error('Cannot remove bug', err)
+        res.status(400).send('Cannot remove bug')
+    })
+})
--- services/bug.service.js
+++ services/bug.service.js
@@ -1,69 +1,56 @@
+import fs from 'fs'
 
-import { storageService } from './async-storage.service.js'
-import { utilService } from './util.service.js'
-
-const STORAGE_KEY = 'bugDB'
-
-_createBugs()
+import { utilService } from "./utils.service.js"
 
 export const bugService = {
     query,
-    getById,
     save,
-    remove,
+    getById,
+    remove
 }
 
+const bugs = utilService.readJsonFile('data/bug.json')
 
 function query() {
-    return storageService.query(STORAGE_KEY)
-}
-function getById(bugId) {
-    return storageService.get(STORAGE_KEY, bugId)
-}
-
-function remove(bugId) {
-    return storageService.remove(STORAGE_KEY, bugId)
+    return Promise.resolve(bugs)
 }
 
 function save(bug) {
     if (bug._id) {
-        return storageService.put(STORAGE_KEY, bug)
-    } else {
-        return storageService.post(STORAGE_KEY, bug)
+        const bugIdx = bugs.findIndex(_bug => _bug._id === bug._id)
+        cars[bugIdx] = bug
     }
+    else {
+        bug._id = utilService.makeId()
+        bug.desc = utilService.makeLorem()
+        bugs.unshift(bug)
+    }
+    return _saveBugsToFile().then(() => bug)
 }
 
+function getById(id) {
+    const bug = bugs.find(bug => bug._id === id)
+    if (!bug) return Promise.reject('bug does not exist!')
+    return Promise.resolve(bug)
+}
 
+function remove(id) {
+    const bugIdx = bugs.findIndex(bug => bug._id === id)
+    if(bugIdx < 0) return Promise.reject('bug does not exist')
+    bugs.splice(bugIdx, 1)
+    return _saveBugsToFile()
+}
 
-
-function _createBugs() {
-    let bugs = utilService.loadFromStorage(STORAGE_KEY)
-    if (!bugs || !bugs.length) {
-        bugs = [
-            {
-                title: "Infinite Loop Detected",
-                severity: 4,
-                _id: "1NF1N1T3"
-            },
-            {
-                title: "Keyboard Not Found",
-                severity: 3,
-                _id: "K3YB0RD"
-            },
-            {
-                title: "404 Coffee Not Found",
-                severity: 2,
-                _id: "C0FF33"
-            },
-            {
-                title: "Unexpected Response",
-                severity: 1,
-                _id: "G0053"
+function _saveBugsToFile() {
+   
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(bugs, null, 4)
+        fs.writeFile('data/bug.json', data, (err) => {
+            if (err) {
+                console.log(err)
+                return reject(err)
             }
-        ]
-        utilService.saveToStorage(STORAGE_KEY, bugs)
-    }
-
-
-
+            resolve()
+        })
+    })
 }
